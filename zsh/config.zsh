fb14935381325e5820341db37b5f56b742a45f1f
Christophe Bliard <christophe.bliard@trux.info>
2013-12-19 23:34:01 +0100

    merge keys.zsh and options.zsh into config.zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,23 +21,31 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
-setopt CORRECT
 setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
+setopt interactivecomments  # can use '#' in interactive shell for comments
+setopt autocd  # autocd directories without need of 'cd' command
+setopt extendedglob  # use extended globbing
+setopt nomatch  # error if filename pattern has no matches
+setopt notify  # report status of background jobs immediately
+
+unsetopt beep # no beep
+
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
 
 zle -N newtab
+
+# Emacs key bindings by default
+bindkey -e
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
@@ -46,3 +54,6 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+bindkey ";5C" forward-word  # Ctrl-left
+bindkey ";5D" backward-word  # Ctrl-right
--- zsh/keys.zsh
+++
@@ -1,6 +0,0 @@
-# Emacs key bindings
-bindkey -e
-
-# Ctrl-left / Ctrl-right to move caret back and forth
-bindkey ";5C" forward-word
-bindkey ";5D" backward-word
