b5316e872b55bf8e2ac7d75ff2c1d3574784c4f2
Christophe Bliard <christophe.bliard@smartesting.com>
2014-01-09 10:03:15 +0100

    do not use bundler plugin: it messes the ruby paths

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -45,7 +45,7 @@ DISABLE_CORRECTION="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(bundler debian git github vagrant cp rails colorize)
+plugins=(debian git github vagrant cp rails colorize)
 
 source $ZSH/oh-my-zsh.sh
 
