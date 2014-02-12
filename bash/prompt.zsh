fd13fb1cb44383280b5a430b31448770f645a0ac
Christophe Bliard <christophe.bliard@smartesting.com>
2014-02-12 17:32:36 +0100

    Ensure FUNCNAME in PS4 is harmless if set -u

--- bash/prompt.zsh
+++ bash/prompt.zsh
@@ -1,3 +1,3 @@
 # stolen from http://binfalse.de/2012/09/howto-debug-bash-scripts/
 # customize trace script bash prompt
-export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}: '
+export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME:+$FUNCNAME[0]:} '
