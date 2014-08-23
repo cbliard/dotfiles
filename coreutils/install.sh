5d7c88b067eb269ff0ca140613b6c28c9f1a5b1a
Christophe Bliard <christophe.bliard@trux.info>
2014-08-23 15:36:50 +0200

    Automagically install some vital packages

---
+++ coreutils/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+packages=()
+
+install_packages() {
+  for package in $* ; do
+    if ! dpkg -s $package &> /dev/null ; then
+      packages+=($package)
+    fi
+  done
+  if [ ${#packages[@]} -gt 0 ] ; then
+    sudo apt-get install ${packages[@]}
+  fi
+}
+
+install_packages curl terminator vim zsh
+
