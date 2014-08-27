9b8fe3bda88ffcea14ac9f73dae070959a696a86
Christophe Bliard <christophe.bliard@smartesting.com>
2014-08-27 16:10:50 +0200

    Be smart with paths

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,10 +2,12 @@
 
 # Adapted from http://powerline.readthedocs.org/en/latest/installation/linux.html
 
+script_dir=$(dirname $0)
+
 mkdir -p $HOME/.fonts
-cp -f *.otf $HOME/.fonts
+cp -f "$script_dir"/*.otf $HOME/.fonts
 fc-cache -vf ~/.fonts
 
 mkdir -p $HOME/.config/fontconfig/conf.d
-cp -f 10-powerline-symbols.conf $HOME/.config/fontconfig/conf.d
+cp -f "$script_dir"/10-powerline-symbols.conf $HOME/.config/fontconfig/conf.d
 
