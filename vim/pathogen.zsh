634639bd57f11cdc378d8ff40d3aed270056ad32
Christophe Bliard <christophe.bliard@smartesting.com>
2014-08-27 16:13:28 +0200

    Fix pathogen installation
    Follow web redirections when downloading the pathogen autoload config
    file.

--- vim/pathogen.zsh
+++ vim/pathogen.zsh
@@ -1,6 +1,7 @@
 if ! [ -e ~/.vim/autoload/pathogen.vim ]; then
+  echo "Vim: installing pathogen autoloading"
   mkdir -p ~/.vim/autoload ~/.vim/bundle
-  curl -Sso ~/.vim/autoload/pathogen.vim \
+  curl -SLso ~/.vim/autoload/pathogen.vim \
       https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
 
