9d3510beb7a89e62cef132b44d8a17d03819b1b0
Christophe Bliard <christophe.bliard@smartesting.com>
2014-09-18 11:13:41 +0200

    Add my favorite vim bundles

--- vim/install.sh
+++ vim/install.sh
@@ -10,7 +10,28 @@ fi
 
 # install bundles
 declare -A bundles
-bundles[vim-colors-solarized]='git://github.com/altercation/vim-colors-solarized.git'
+# Tree explorer plugin
+bundles[nerdtree]='https://github.com/scrooloose/nerdtree.git'
+# Easily search for, substitute, and abbreviate multiple variants of a word
+# Subvertion: :S/blog{,s}/post{,s}/g => like :s but smarter
+# Coercion: crs => snake_case; crc => camelCase; crm => MixedCase; cru => UPPER_CASE
+bundles[vim-abolish]='https://github.com/tpope/vim-abolish.git'
+# Lean & mean status/tabline for vim that's light as air
+bundles[vim-airline]='https://github.com/bling/vim-airline'
+# Solarized color theme
+bundles[vim-colors-solarized]='https://github.com/altercation/vim-colors-solarized.git'
+# Awesome git wrapper. :G<command>
+bundles[vim-fugitive]='https://github.com/tpope/vim-fugitive.git'
+# Shows a git diff in the gutter (sign column) and stages/reverts hunks
+bundles[vim-gitgutter]='https://github.com/airblade/vim-gitgutter.git'
+# Enable repeating supported plugin maps with "."
+bundles[vim-repeat]='https://github.com/tpope/vim-repeat.git'
+# Defaults everyone can agree on
+bundles[vim-sensible]='https://github.com/tpope/vim-sensible.git'
+# Heuristically set buffer options
+bundles[vim-sleuth]='https://github.com/tpope/vim-sleuth.git'
+# Quoting/parenthesizing made simple. cs'": change quotes ' to "
+bundles[vim-surround]='https://github.com/tpope/vim-surround.git'
 
 mkdir -p $HOME/.vim/bundle
 cd $HOME/.vim/bundle
