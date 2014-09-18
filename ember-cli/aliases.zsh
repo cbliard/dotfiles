4295bace00ec548fbdee65c69d18bfe3c4dc1d06
Christophe Bliard <christophe.bliard@trux.info>
2014-09-18 22:31:59 +0200

    Add ember-cli alias, using docker image & fig

---
+++ ember-cli/aliases.zsh
@@ -0,0 +1,4 @@
+# Use fig and visualjeff/ember-cli docker image to run ember-cli
+alias npm='fig --file ~/.ember-cli.fig.yml run --rm npm'
+alias bower='fig --file ~/.ember-cli.fig.yml run --rm bower'
+alias ember='fig --file ~/.ember-cli.fig.yml run --rm ember'
