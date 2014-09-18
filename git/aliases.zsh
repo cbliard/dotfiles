e1fe4043ef6929fc132e6b01ab59f7cca5dc9629
Christophe Bliard <christophe.bliard@trux.info>
2014-09-18 23:08:18 +0200

    git: override some git aliases
    I will not mess anymore wanting to do git log and doing a git pull
    instead...
    gl=git log
    gph=git push
    gpl=git pull
    
    also moved other git aliases in the oh-my-zsh custom plugin section

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,12 +6,3 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-compdef _git gs=git-status
-alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
-# git log diff from last pull
-alias gld='git log --color --reverse --patch --stat --abbrev-commit --date=relative --decorate ORIG_HEAD..'
-compdef _git gld=git-log
---
+++ zsh/oh-my-zsh-custom/plugins/git-overrides/git-overrides.plugin.zsh
@@ -0,0 +1,26 @@
+# Change some aliases from oh-my-zsh git plugin
+
+# Return if not sourced by oh-my-zsh
+[ -n "$ZSH_CUSTOM" ] || return 0
+
+# Change git pull and gitpush
+unalias gl # I often intend to do git log, but does git pull
+compdef -d gl
+unalias gp
+compdef -d gp
+alias gph='git push'
+compdef _git gph=git-push
+alias gpl='git pull'
+compdef _git gpl=git-pull
+alias gl='git log'
+compdef _git gl=git-log
+
+# The rest of my fun git aliases
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+compdef _git gs=git-status
+alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
+           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+# git log diff from last pull
+alias gld='git log --color --reverse --patch --stat --abbrev-commit --date=relative --decorate ORIG_HEAD..'
+compdef _git gld=git-log
