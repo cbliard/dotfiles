2a6e47ee01e49825f164abc825556a018b88870b
Christophe Bliard <christophe.bliard@trux.info>
2014-12-01 15:55:13 +0100

    Do not load rvm if inside a docker container

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -1,1 +1,6 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+# do not load rvm when in docker container
+if ! [ -e /.dockerinit ]
+then
+  export PATH="$PATH:$HOME/.rvm/bin"  # Add RVM to PATH for scripting
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+fi
