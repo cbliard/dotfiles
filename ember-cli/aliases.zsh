279ed827cd398bf5e4290a85a65ed180d485bbc7
Christophe Bliard <christophe.bliard@smartesting.com>
2015-01-30 09:23:48 +0100

    disable aliases npm, bower and ember

--- ember-cli/aliases.zsh
+++ ember-cli/aliases.zsh
@@ -1,4 +1,6 @@
 # Use fig and visualjeff/ember-cli docker image to run ember-cli
-alias npm='fig --file ~/.ember-cli.fig.yml run --rm npm'
-alias bower='fig --file ~/.ember-cli.fig.yml run --rm bower'
-alias ember='fig --file ~/.ember-cli.fig.yml run --rm ember'
+
+# Does not work correctly, temporarly disabled, waiting for better idea
+#alias npm='fig --file ~/.ember-cli.fig.yml run --rm npm'
+#alias bower='fig --file ~/.ember-cli.fig.yml run --rm bower'
+#alias ember='fig --file ~/.ember-cli.fig.yml run --rm ember'
