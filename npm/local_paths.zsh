a0663b5d7d1cc38110818ef76f89fa5d7eef760d
Christophe Bliard <christophe.bliard@trux.info>
2015-03-08 13:41:46 +0100

    Install global npm modules in home dir
    Thanks to the StackOverflow top answer on
    http://stackoverflow.com/questions/10081293/install-npm-into-home-directory-with-distribution-nodejs-package-ubuntu

---
+++ npm/local_paths.zsh
@@ -0,0 +1,15 @@
+# Taken from the StackOverflow top answer at
+# http://stackoverflow.com/questions/10081293/install-npm-into-home-directory-with-distribution-nodejs-package-ubuntu
+
+# NPM packages in homedir
+NPM_PACKAGES="$HOME/.npm-packages"
+
+# Tell our environment about user-installed node tools
+PATH="$NPM_PACKAGES/bin:$PATH"
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH
+MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
+# Tell Node about these packages
+NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+
