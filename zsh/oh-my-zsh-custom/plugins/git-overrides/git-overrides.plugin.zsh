29877b10af9d581d4501fb8711b4eb182e9f429f
Christophe Bliard <christophe.bliard@smartesting.com>
2015-06-18 10:56:27 +0200

    zsh/git: remove unneeded compdef

--- zsh/oh-my-zsh-custom/plugins/git-overrides/git-overrides.plugin.zsh
+++ zsh/oh-my-zsh-custom/plugins/git-overrides/git-overrides.plugin.zsh
@@ -10,22 +10,15 @@ alias gdc='git diff --cached'
 
 # Change git pull and gitpush
 unalias gl &> /dev/null # I often intend to do git log, but does git pull
-compdef -d gl
 unalias gp &> /dev/null
-compdef -d gp
 alias gph='git push'
-compdef _git gph=git-push
 alias gpl='git pull'
-compdef _git gpl=git-pull
 alias gl='git log'
-compdef _git gl=git-log
 
 # The rest of my fun git aliases
 alias gaa='git add --all'
-compdef _git gaa=git-add
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-compdef _git gs=git-status
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 # git log diff from last pull
