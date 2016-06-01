bbfeff4a6d55521c3b25f87a6d033b1932c46802
Christophe Bliard <christophe.bliard@hiptest.net>
2016-06-01 11:02:49 +0200

    Fix $PATH to include $HOME/bin

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,8 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
-export PATH="$HOME/Christophe/bin:$PATH"
-export PATH="$HOME/Christophe/apps/play:$PATH"
+if [ -d "$HOME/Christophe/bin" ]; then
+  export PATH="$HOME/Christophe/bin:$PATH"
+elif [ -d "$HOME/bin" ]; then
+  export PATH="$HOME/bin:$PATH"
+fi
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
