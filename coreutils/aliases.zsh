009dcf5c310849c30ee54ab66697ba442d67d951
Christophe Bliard <christophe.bliard@hiptest.net>
2016-08-24 14:29:53 +0200

    Add NC zsh alias (No Color)

--- coreutils/aliases.zsh
+++ coreutils/aliases.zsh
@@ -18,3 +18,4 @@ alias -g X='| xargs'
 alias -g A1='| awk '\''{print $1}'\'
 alias -g A2='| awk '\''{print $2}'\'
 alias -g A3='| awk '\''{print $3}'\'
+alias -g NC='| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"'  # remove color
