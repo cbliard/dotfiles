d2c3b3857a7c2494d7dc8a0272e7defc86226e2f
Christophe Bliard <christophe.bliard@hiptest.net>
2017-01-17 10:15:41 +0100

    Add Meta-S -> sudo + ... -> ../.. keybindings
    Thanks to
    http://linuxfr.org/users/chimay/journaux/alterner-les-repertoires-avec-zsh#comment-1689178

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -21,3 +21,27 @@ bindkey -M menuselect '^?' undo
 # group completion matches by their type
 zstyle ':completion:*:descriptions' format '%B%U%d%b%u'
 zstyle ':completion:*' group-name ''
+
+# Meta-S will toggle sudo
+function vbe-sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N vbe-sudo-command-line
+bindkey "\es" vbe-sudo-command-line
+
+# Expand ... to ../..
+function vbe-expand-dot-to-parent-directory-path() {
+  case $LBUFFER in
+    (./..|* ./..) LBUFFER+='.' ;; # In Go: "go list ./..."
+    (..|*[ /=]..) LBUFFER+='/..' ;;
+    (*) LBUFFER+='.' ;;
+  esac
+}
+zle -N vbe-expand-dot-to-parent-directory-path
+bindkey "." vbe-expand-dot-to-parent-directory-path
+bindkey -M isearch "." self-insert
