400ea4d34d321284d5b6609d5d6f2eab6157cd7a
Christophe Bliard <christophe.bliard@trux.info>
2017-12-21 18:11:21 +0100

    add database_url segment to powerline shell

---
+++ powershell/database_url.py
@@ -0,0 +1,24 @@
+from ..utils import BasicSegment, warn
+import os
+import re
+
+LOW_BATTERY_THRESHOLD = 20
+
+
+class Segment(BasicSegment):
+	def add_to_powerline(self):
+		# See discussion in https://github.com/banga/powerline-shell/pull/204
+		# regarding the directory where battery info is saved
+		if not os.environ.has_key('DATABASE_URL'):
+			return
+		databaseUrl = os.environ['DATABASE_URL']
+		if not databaseUrl:
+			return
+		m = re.search(r"^postgres://([^:@/]+(:[^@/]+)?@)?[^:/]+(:\d+)?/(\w+)$", databaseUrl)
+		if not m:
+			return
+		databaseName = m.group(4)
+		if databaseName:
+			self.powerline.append(" db:%s " % os.path.basename(databaseName),
+									self.powerline.theme.AWS_PROFILE_FG,
+									self.powerline.theme.AWS_PROFILE_BG)
--- powershell/install.sh
+++ powershell/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 pip2 install powerline-shell
+
+if [ -d /usr/local/lib/python2.7/site-packages/powerline_shell/segments ]
+then
+  cp database_url.py /usr/local/lib/python2.7/site-packages/powerline_shell/segments/database_url.py
+fi
