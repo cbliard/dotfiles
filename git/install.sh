e8292bda5ded504c54516f95f1ff5743a817adf3
Christophe Bliard <christophe.bliard@hiptest.net>
2018-02-21 10:33:21 +0100

    [git] Use abbrev commit

--- git/install.sh
+++ git/install.sh
@@ -7,8 +7,9 @@ git config --global color.status auto
 git config --global color.ui true
 git config --global core.editor vim
 git config --global core.excludesfile $HOME/.gitignore
-git config --global push.default simple
 git config --global fetch.prune true
+git config --global log.abbrevCommit true
+git config --global push.default simple
 
 # Three ways diff, so we have the common text
 git config --global merge.conflict diff3
