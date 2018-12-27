9b2103d70a60d5502065d9b6e8fe266bd9e45e0e
Christophe Bliard <christophe.bliard@hiptest.net>
2018-12-27 10:11:05 +0100

    ✨ add script to fix WM keyboard shortcuts
    Ctrl-Alt-arrows are used by Window Manager to switch workspaces, but this
    conflicts with VS Code shortcuts. Remap them to Win+PgUp.
    
    This script is here for my future me :)

---
+++ system/fix_keyboard_shortcuts.sh
@@ -0,0 +1,10 @@
+# Ctrl-Alt-arrows are used by Window Manager to switch workspaces, but this
+# conflicts with VS Code shortcuts. Remap them to Win+PgUp
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "[]"
