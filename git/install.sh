766acb80cc7dc2356ed29df3cdd26cea2e652b45
Christophe Bliard <christophe.bliard@trux.info>
2019-09-18 11:04:07 +0200

    ✨ Make vscode the default git editor

--- git/install.sh
+++ git/install.sh
@@ -5,7 +5,7 @@ git config --global color.branch auto
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.ui true
-git config --global core.editor vim
+git config --global core.editor "code --wait"
 git config --global core.excludesfile $HOME/.gitignore
 git config --global fetch.prune true
 git config --global log.abbrevCommit true
