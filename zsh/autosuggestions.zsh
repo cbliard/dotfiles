65d082528ce8e26f913aea5d70abfdfdf1f952db
Christophe Bliard <christophe.bliard@trux.info>
2020-09-07 10:23:53 +0200

    remove fish-like autosuggestions

--- zsh/autosuggestions.zsh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "$HOME/.zsh-autosuggestions" ]
-then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh-autosuggestions"
-fi
-
-ZSH_AUTOSUGGEST_STRATEGY=history
-ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=50
-
-source "$HOME/.zsh-autosuggestions/zsh-autosuggestions.zsh"
