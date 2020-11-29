0f7268a56ac23ab2441639d33624514c45601947
Christophe Bliard <christophe.bliard@trux.info>
2020-11-29 12:37:06 +0100

    Use powerline10k (zsh) instead of powerline-shell (go)

---
+++ shell-prompt/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ ! -d "$HOME/.powerlevel10k" ];
+then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.powerlevel10k"
+fi
---
+++ shell-prompt/prompt.zsh
@@ -0,0 +1,2 @@
+# Using powerline10k
+source ~/.powerlevel10k/powerlevel10k.zsh-theme
