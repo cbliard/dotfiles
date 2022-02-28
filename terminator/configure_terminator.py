22d8ac609ce35ca2c0b08a15316083e8dac6fa21
Christophe Bliard <christophe.bliard@trux.info>
2022-02-28 16:22:38 +0100

    better regexp to open file from terminator

--- terminator/configure_terminator.py
+++ terminator/configure_terminator.py
@@ -26,6 +26,9 @@ if not 'EditorPlugin' in config['enabled_plugins']:
     config['enabled_plugins'].insert(0, 'EditorPlugin')
 config.plugin_set('EditorPlugin', 'command', r'code --goto {filepath}:{line}:{column}')
 config.plugin_set('EditorPlugin', 'groups', 'file line column')
-config.plugin_set('EditorPlugin', 'match', r'([^ \t\n\r\f\v:]+?):([0-9]+):?([0-9]+)?')
+# previous regexp was config.plugin_set('EditorPlugin', 'match', r'([^ \t\n\r\f\v:]+?):([0-9]+):?([0-9]+)?')
+# new one is /(?:[ab]\/)?((?:[^'"\[ \t\n\r\f\v:]+[\/\.])+[a-zA-Z_]+)(?::([0-9]+))?(?::([0-9]+))?/gm
+# able to match files without line number, and the a//some/file/path displayed in git diffs
+config.plugin_set('EditorPlugin', 'match', r'(?:[ab]\/)?((?:[^\'"\[ \t\n\r\f\v:]+[\/\.])+[a-zA-Z_]+)(?::([0-9]+))?(?::([0-9]+))?')
 
 config.save()
