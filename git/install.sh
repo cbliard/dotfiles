386226688bde6d2aa4e8f571aa5fc08d7388852e
Christophe Bliard <christophe.bliard@trux.info>
2022-04-28 15:40:56 +0200

    configure colors for diff-so-fancy use in git

--- git/install.sh
+++ git/install.sh
@@ -14,3 +14,22 @@ git config --global push.default simple
 
 # Three ways diff, so we have the common text
 git config --global merge.conflict diff3
+
+# Recommended default colors for diff-so-fancy
+# --------------------------------------------
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "white bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "white bold 22"
+
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RXg"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
