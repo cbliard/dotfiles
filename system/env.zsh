cce19c0b3f25b97796e9cac4b8d158946f0de625
Christophe Bliard <christophe.bliard@trux.info>
2022-12-22 13:36:47 +0100

    fix EDITOR command for vscode

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,5 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
   # Use vscode for my editor.
-  export EDITOR='code'
+  export EDITOR='code --wait'
 fi
