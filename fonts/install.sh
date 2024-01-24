f62919010e458984182f1da07da9c1466beb8b66
Christophe Bliard <cbliard@cbl-macbook.local>
2024-01-24 17:42:47 +0100

    Install Nerd fonts to fix p10k prompt

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,11 +2,19 @@
 
 # Adapted from http://powerline.readthedocs.org/en/latest/installation/linux.html
 
-script_dir=$(dirname $0)
+lib_dir="$(cd "$(dirname $0)"/../lib && pwd)"
+source "$lib_dir/support.sh"
 
-if ! [ -d "$HOME/code/github/fonts" ]
-then
-  echo "Installing Powerline fonts."
-  git clone https://github.com/powerline/fonts.git --depth=1 "$HOME/code/github/fonts"
-  $HOME/code/github/fonts/install.sh
+if is_osx ; then
+  brew tap homebrew/cask-fonts
+  install_osx_packages font-hack-nerd-font font-meslo-lg-nerd-font font-monaspace-nerd-font
+else
+  # TODO: install some Nerd fonts instead of Powerline fonts
+  # https://www.nerdfonts.com/font-downloads
+  if ! [ -d "$HOME/code/github/fonts" ]
+  then
+    echo "Installing Powerline fonts."
+    git clone https://github.com/powerline/fonts.git --depth=1 "$HOME/code/github/fonts"
+    $HOME/code/github/fonts/install.sh
+  fi
 fi
