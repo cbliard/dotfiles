f3a48b6cd2974142f8f286b41dc4033a1633b53e
Christophe Bliard <cbliard@cbl-macbook.local>
2024-01-24 17:42:13 +0100

    Install fzf and ripgrep on osx

--- coreutils/install.sh
+++ coreutils/install.sh
@@ -4,4 +4,4 @@ lib_dir="$(cd "$(dirname $0)"/../lib && pwd)"
 source "$lib_dir/support.sh"
 
 install_linux_packages colordiff curl fd-find fzf terminator vim ripgrep zsh python3-pip
-install_osx_packages curl bash zsh vim coreutils
+install_osx_packages coreutils curl bash fzf ripgrep vim zsh
