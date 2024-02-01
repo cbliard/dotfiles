ea40ec3111f07a6d4ba9450df0700a2329ea6c22
Christophe Bliard <christophe.bliard@trux.info>
2024-02-01 18:19:55 +0100

    Add fd to macos packages
    fd is a simple, fast and user-friendly alternative to find.

--- coreutils/install.sh
+++ coreutils/install.sh
@@ -4,4 +4,4 @@ lib_dir="$(cd "$(dirname $0)"/../lib && pwd)"
 source "$lib_dir/support.sh"
 
 install_linux_packages colordiff curl fd-find fzf terminator vim ripgrep zsh python3-pip
-install_osx_packages bash bat coreutils curl fzf ripgrep vim watchexec zsh
+install_osx_packages bash bat coreutils curl fd fzf ripgrep vim watchexec zsh
